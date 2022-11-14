5fbae6b6aff6c4debd31838ca7fbb969d97649e3
aibreich <aibreich@iu.edu>
2022-11-14 17:52:10 -0500

    Initial commit

---
+++ application/config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Author: Aiden Eichenour
+ * Date: Date: 11/14/2022
+ * File: config.php
+ * Description: set application settings
+ * 
+ */
+
+//error reporting level: 0 to turn off all error reporting; E_ALL to report all
+error_reporting(E_ALL);
+
+//local time zone
+date_default_timezone_set('America/New_York');
+
+//base url of the application
+define("BASE_URL", "http://localhost/i211/FinalProject");
+
+
+/*************************************************************************************
+ *                       settings for Store                                        *
+ ************************************************************************************/
+
+//define default path for media images
+//define("MOVIE_IMG", "www/img/movies/");
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Author: Aiden Eichenour
+ * date: 11/14/22
+ * File: index.php
+ * Description: Final Project- One Stop Sausage. index page
+ */
