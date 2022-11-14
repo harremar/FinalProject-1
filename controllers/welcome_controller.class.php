11b75d27243fafc28fe9d4e1b0408db73533d5c1
aibreich <aibreich@iu.edu>
2022-11-14 17:57:43 -0500

    update

---
+++ controllers/welcome_controller.class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Author: Aiden Eichenour
+ * date: 11/14/22
+ * File: welcome_controller.class.php
+ * Description:
+ */
---
+++ views/index_view.class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Author: Aiden Eichenour
+ * date: 11/14/22
+ * File: index_view.class.php
+ * Description:
+ */
