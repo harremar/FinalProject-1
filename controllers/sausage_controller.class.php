060b8195cd809b728eace944d0300aefef61e638
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-11-15 20:24:47 -0500

    Update sausage_controller.class.php

--- controllers/sausage_controller.class.php
+++ controllers/sausage_controller.class.php
@@ -3,5 +3,36 @@
  * Author: Aiden Eichenour
  * date: 11/14/22
  * File: sausage_controller.class.php
- * Description:
+ * Description: not done yet dffdfsdfsddds
  */
+
+class SausageController
+{
+    private $toy_model;
+
+    public function __construct(){
+        $this->toy_model = new SausageModel();
+    }
+
+    //list all toys
+    public function all(){
+        //get all the toys
+        $toys=$this->toy_model->getSausages();
+
+        //handle errors
+        if(!$toys){
+            $this->error("No sausage was found");
+        }
+
+        //display all the toys
+        $view = new SausageView();
+        $view->display($toys);
+    }
+
+
+    //display an error
+    public function error($message){
+        $view = new SausageError();
+        $view->display($message);
+    }
+}
