0c817252c2a7b8c9a9d5862bd1bd8e5720014cee
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-11-17 11:23:45 -0500

    Update database.class.php

--- application/database.class.php
+++ application/database.class.php
@@ -8,7 +8,8 @@
  * 
  */
 
-class Database {
+class Database
+{
 
     //define database parameters
     private $param = array(
@@ -16,64 +17,46 @@ class Database {
         'login' => 'phpuser',
         'password' => 'phpuser',
         'database' => 'one_stop_sausage',
-        'tblOrders' => 'orders',
-        'tblOrders_line_items' => 'Orders_line_items',
-        'tblsausage' => 'sausage',
-        'tbluser' => 'user'
+        'tblSausage' => 'sausage'
     );
     //define the database connection object
     private $objDBConnection = NULL;
     static private $_instance = NULL;
 
     //constructor
-    private function __construct() {
-        try{
-            $this->objDBConnection = new mysqli(
-                $this->param['host'], $this->param['login'], $this->param['password'], $this->param['database']
-            );
-            if (mysqli_connect_errno() != 0) {
-                throw new DatabaseConnectionException("Connecting database failed: " . mysqli_connect_error() . ".");
-            }
-        }catch (DatabaseConnectionException $e) {
-            $view = new SausageError();
-            $view->display($e->getMessage());
-        }catch (Exception $e) {
-            $view = new SausageError();
-            $view->display($e->getMessage());
-        }
+    public function __construct()
+    {
 
+        $this->objDBConnection = @new mysqli(
+            $this->param['host'],
+            $this->param['login'],
+            $this->param['password'],
+            $this->param['database']
+        );
+        if (mysqli_connect_errno() != 0) {
+            echo "Connecting to database failed: " . mysqli_connect_error();
+            exit();
+        }
     }
 
     //static method to ensure there is just one Database instance
-    static public function getDatabase() {
+    static public function getInstance()
+    {
         if (self::$_instance == NULL)
             self::$_instance = new Database();
         return self::$_instance;
     }
 
     //this function returns the database connection object
-    public function getConnection() {
+    public function getConnection()
+    {
         return $this->objDBConnection;
     }
 
-    //returns the name of the table that stores orders
-    public function getOrderTable() {
-        return $this->param['tblOrders'];
-    }
-
-    //returns the name of the table that stores orders of line items
-    public function getOrderLineItems() {
-        return $this->param['tblOrders_line_items'];
-    }
-
-    //returns the name of the table storing sausages
-    public function getSausageTable() {
-        return $this->param['tblsausage'];
-    }
-
-    //returns the name of the table storing users
-    public function getUserTable() {
-        return $this->param['tbluser'];
+    //returns the name of the table storing books
+    public function getTable()
+    {
+        return $this->param['tblSausage'];
     }
 
 }
