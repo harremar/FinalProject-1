6c78e01e4c2ca9e2aa0a2722b2be525c2659df21
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-11-17 17:16:17 -0500

    Update welcome_controller.class.php

--- controllers/welcome_controller.class.php
+++ controllers/welcome_controller.class.php
@@ -5,3 +5,12 @@
  * File: welcome_controller.class.php
  * Description:
  */
+class WelcomeController {
+    //put your code here
+    public function index() {
+        $view = new WelcomeIndex();
+        $model = new SausageModel();
+        $sausages = $model->getSausages();
+        $view->display($sausages);
+    }
+}
