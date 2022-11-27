d03e5ac49e62c7eaf7a3538022ab42d3f7d1924b
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-11-27 16:53:28 -0500

    Update sausage_detail.class.php

--- views/detail/sausage_detail.class.php
+++ views/detail/sausage_detail.class.php
@@ -5,3 +5,24 @@
  * File: sausage_detail.class.php
  * Description:
  */
+
+class Details extends SausageView
+{
+    public function display($sausage){
+        //Display the header
+        parent::displayHeader("One Stop Sausage Details Page ");
+
+        //retrieve item details by calling get methods
+        $id = $sausage->getId();
+        echo $id;
+
+        ?>
+        <div> this will display the details page </div>
+        <a href='<?= BASE_URL ?>/welcome'>back</a>
+
+        <?php
+        //Display the footer
+        parent::displayFooter();
+
+    }
+}
