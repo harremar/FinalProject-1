a2a05b0b9800757cbb90f30888e7f1991a635d5f
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-11-27 17:00:18 -0500

    Update welcome_controller.class.php

--- controllers/welcome_controller.class.php
+++ controllers/welcome_controller.class.php
@@ -3,14 +3,62 @@
  * Author: Aiden Eichenour
  * date: 11/14/22
  * File: welcome_controller.class.php
- * Description:
+ * Description: this is the user controller class which defines the 8 actions
  */
 class WelcomeController {
-    //code for the welcome controller
+
+    public $item_model;
+
+    //default constructor
+    public function __construct() {
+        //create an instance of the MovieModel class
+        $this->item_model = SausageModel::getSausageModel();
+    }
+
+    //index
     public function index() {
         $view = new WelcomeIndex();
         $model = new SausageModel();
         $sausages = $model->getSausages();
         $view->display($sausages);
+    }
+
+    //login
+    public function login(){
+        //display
+        $view = new Login();
+        $view->display();
+    }
+
+    //search
+    public function search(){
+        //display
+        $view = new Search();
+        $view->display();
+    }
+
+    //details
+    public function details($id){
+        //retrieve details of the item
+        $item = $this->item_model->view_item($id);
+        if(!$item){
+            //display an error message
+            $message = "there was a problem displaying the movie id = $id.";
+            $this->error($message);
+            return;
+        }
+        //display
+        $view = new Details();
+        $view->display($item);
+    }
+
+
+    //handle an error
+    public function error($message) {
+        //create an object of the error class
+        $error = new SausageError();
+
+        //display the error message
+        $error->display($message);
     }
 }
