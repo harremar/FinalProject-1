0e36561997c9f535e5dfeaa913a9d90270cb7b2f
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-12-01 13:09:14 -0500

    Update sausageModel.class.php

--- models/sausageModel.class.php
+++ models/sausageModel.class.php
@@ -12,6 +12,7 @@ class SausageModel
     private $dbConnection; //database connection
     static private $_instance = NULL;
     private $tblSausage;
+    private $tblUser;
 
     //making the constructor
     public function  __construct()
@@ -19,6 +20,7 @@ class SausageModel
         $this->db = Database::getInstance();
         $this->dbConnection = $this->db->getConnection();
         $this->tblSausage = $this->db->getSausageTable();
+        $this->tblUser = $this->db->getUserTable();
 
         //Escapes special characters in a string for use in an SQL statement. This stops SQL inject in POST vars.
         foreach ($_POST as $key => $value) {
@@ -139,5 +141,112 @@ class SausageModel
             $items[] = $item;
         }
         return $items;
+    }
+    //add a user into the "users" table in the database
+    public function add_user() {
+        //retrieve user inputs from the registration form
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+        $password = trim(filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
+        $lastname = filter_input(INPUT_POST, "lname", FILTER_SANITIZE_STRING);
+        $firstname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_EMAIL);
+        $city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_EMAIL);
+        $state = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_EMAIL);
+        $country = filter_input(INPUT_POST, 'country', FILTER_SANITIZE_EMAIL);
+        $zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_EMAIL);
+        $card_number = filter_input(INPUT_POST, 'card_number', FILTER_SANITIZE_EMAIL);
+        $card_cvc = filter_input(INPUT_POST, 'card_cvc', FILTER_SANITIZE_EMAIL);
+
+
+
+
+        try {
+            //Handle data missing exceptions. All fields are required.
+            if (empty($username) || empty($password) || empty($lastname) || empty($firstname) || empty($email)) {
+                throw new DataMissingException("Values were missing in one or more fields. All fields must be filled.");
+            }
+
+            //Handle data length exceptions. The min length of a password is 5.
+            if (strlen($password) < 5) {
+                throw new DataLengthException("Your password was invalid. The mininum length of a password is 5.");
+            }
+
+            //Handle email format exceptions.
+            if (!Utilities::checkemail($email)) {
+                throw new EmailFormatException("Your email format was invalid. The general format of an email address is user@example.com.");
+            }
+            //hash the password
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            //construct an INSERT query
+            $sql = "INSERT INTO " . $this->db->getUserTable() . " VALUES(NULL, '0', '$firstname $lastname', '$username', '$hashed_password', '$address', '$city', '$state', '$country', '$zipcode', '$card_number', '$card_cvc')";
+
+            //Execute the query. Throw a database exceptions if the query failed.
+            if ($this->dbConnection->query($sql) === FALSE) {
+                throw new DatabaseException("We are sorry, but we can't create your account at this moment. Please try again later.");
+            }
+
+            return "Your account has been successfully created.";
+        } catch (DataMissingException $e) {
+            return $e->getMessage();
+        } catch (DataLengthException $e) {
+            return $e->getMessage();
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (EmailFormatException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //verify username and password against a database record
+    public function verify_user() {
+        //retrieve username and password
+        $username = trim(filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING));
+        $password = trim(filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
+
+        try {
+            //Handle data missing exception. All fields are required.
+            if (empty($username) || empty($password)) {
+                throw new DataMissingException("Values were missing in one or more fields. All fields must be filled.");
+            }
+
+            //sql statement to filter the users table data with a username
+            $sql = "SELECT user_password FROM " . $this->db->getUserTable() . " WHERE user_username='$username'";
+
+            //execute the query
+            $query = $this->dbConnection->query($sql);
+
+            if (!$query) {
+                throw new DatabaseException("We are sorry, but we cannot create your account at this moment. Please try again later.");
+            }
+            //verify password; if password is valid, set a temporary cookie
+            if ($query->num_rows > 0) {
+                $result_row = $query->fetch_assoc();
+                $hash = $result_row['user_password'];
+                if (password_verify($password, $hash)) {
+                    setcookie("user", $username);
+                    return "You have successfully logged in.";
+                } else {
+                    throw new DatabaseException("Your username and/or password were invalid. Please try again.");
+                }
+            } else {
+                throw new DatabaseException("Your username and/or password were invalid. Please try again.");
+            }
+        } catch (DataMissingException $e) {
+            return $e->getMessage();
+        } catch (DatabaseException $e) {
+            return $e->getMessage();
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    //logout user: destroy session data
+    public function logout() {
+        //destroy session data
+        setcookie("user", '', -10);
+        return true;
     }
 }
