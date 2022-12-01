700cf5298f33c79e00e03736277c063b0385b35d
Marielle R Harrell <78804575+harremar@users.noreply.github.com>
2022-12-01 13:25:55 -0500

    Update createAccount.php

--- views/items/createAccount/createAccount.php
+++ views/items/createAccount/createAccount.php
@@ -1,7 +1,37 @@
 <?php
 /**
- * Author: Aiden Eichenour
- * date: 12/1/22
- * File: createAccount.php
- * Description:
+ *Author: Marielle Harrell
+ *Date: 11/29/2022
+ *File: createAccount.class.php
+ *Description:
  */
+
+class Create extends SausageView {
+
+    public function display() {
+        parent::displayHeader("this is the createAccount page");
+        ?>
+        <div class="loginContainer">
+            <div class="greyBar"> Create an Account</div>
+            <div class="middle-row">
+                <p style="padding-top: 15px">Please complete the entire form. All fields are required.</p>
+                <form method="post" action="<?= BASE_URL ?>/welcome/register">
+                    <div><input class="input" type="text" name="username" style="width: 99%"  placeholder="Username"></div>
+                    <div><input class="input" type="password" name="password" style="width: 99%"  placeholder="Password, 5 characters minimum"></div>
+                    <div><input class="input" type="text" name="email" style="width: 99%" placeholder="Email"></div>
+                    <div><input class="input" type = 'text' name="fname" style="width: 99%"  placeholder="First name"></div>
+                    <div><input class="input" type="text" name="lname" style="width: 99%" placeholder="Last name"></div>
+                    <div><input class="submitButton" type="submit" class="button" value="Register"></div>
+                </form>
+            </div>
+            <div class="bottom-row">
+                <span style="float: left; font-family: Gadugi">Already have an account? <a href="<?= BASE_URL ?>/welcome/login">Login</a></span>
+                    <br/>
+                <a style="text-decoration: none" href='<?= BASE_URL ?>/welcome'><div class="backButton"><- Back to Home</div></a>
+            </div>
+            </div>
+        <?php
+        parent::displayFooter();
+    }
+
+}
